e600856ed46c66feafea0ee41bb5cdcb8f030a18
Christos A. Papalitsas <cpapalitsas@gmail.com>
2019-07-03 23:40:34 +0300

    Delete neighborJoinTree.m

--- neighborJoinTree.m
+++
@@ -1,5 +0,0 @@
-PhyloTree2 = seqneighjoin(DD,'equivar',seqs);
-SeqsMultiAligned3 = multialign(seqs, PhyloTree2,'ScoringMatrix','GONNET','GAPOPEN', 1);
-if writeTrees==1
-    phytreewrite('neighborjoin.tree',PhyloTree2);
-end
